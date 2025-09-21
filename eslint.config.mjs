f054f51ecf170644d0ff01a3757c8329226d263a
Mikael H <mikael@hatanpaa.se>
2025-09-21 22:48:16 +0200

    fixed eslint to align with prettier formatting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,7 +24,9 @@ export default tseslint.config(
     },
     rules: {
       ...eslintPluginReactHooks.configs.recommended.rules,
-      ...eslintPluginReactRefresh.configs.vite.rules
+      ...eslintPluginReactRefresh.configs.vite.rules,
+      'prettier/prettier': ['error', {}, { usePrettierrc: true }]
+
     }
   },
   eslintConfigPrettier
